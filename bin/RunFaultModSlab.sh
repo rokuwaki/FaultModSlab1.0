e613abd8bc564c6ad96f4887e08c32a17e524fdf
Ryo Okuwaki <rokuwaki@gmail.com>
2017-01-06 00:00:41 +0900

    Update RunFaultModSlab.sh

--- bin/RunFaultModSlab.sh
+++ bin/RunFaultModSlab.sh
@@ -1,21 +1,22 @@
 #!/usr/local/bin/bash
 
+lat0=46.592
+lon0=153.266
+
 mkdir -p data
 Region=kur #alu, mex, cas, izu, ker, kur, phi, ryu, van, sco, sol, sam, sum
+           #http://earthquake.usgs.gov/data/slab/
 wget https://earthquake.usgs.gov/data/slab/models/$Region"_slab1.0_clip.xyz"    -P data/
 wget https://earthquake.usgs.gov/data/slab/models/$Region"_slab1.0_strclip.xyz" -P data/
 wget https://earthquake.usgs.gov/data/slab/models/$Region"_slab1.0_dipclip.xyz" -P data/
 wget https://earthquake.usgs.gov/data/slab/models/$Region"_top.in"              -P data/
-
-lat0=46.592
-lon0=153.266
 
 depthdata='"data/'$Region'_slab1.0_clip.xyz"'
 strikedata='"data/'$Region'_slab1.0_strclip.xyz"'
 dipdata='"data/'$Region'_slab1.0_dipclip.xyz"'
 
 mkdir -p work
-echo "46.592  153.266  0"               > work/input.dat
+echo $lat0 $lon0 "0"               > work/input.dat
 echo "48.9  -71.7  0.750"              >> work/input.dat
 echo "2.0  2.0  151  151  76  76  225" >> work/input.dat
 echo $depthdata                        >> work/input.dat
