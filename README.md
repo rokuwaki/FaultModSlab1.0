49cd1b5daed09022a1a98f5853d62e806aa255ab
Ryo Okuwaki <rokuwaki@gmail.com>
2017-07-13 00:20:40 +0900

    Update README

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # FaultModSlab1.0
 
-- Codes for generating non-planar fault model by interpolating slab1.0 model ([Hayes et al., 2011, JGR](https://earthquake.usgs.gov/data/slab/))
-- Conversion between local cartesian and geographic coordinates is performed with [GeographicLib](http://geographiclib.sourceforge.net). Converted (to Fortran90) version of geographiclib is used (provided by [alex-robinson/coordinates](https://github.com/alex-robinson/coordinates)) for subroutine. 
-- Rake angle for each source knot is assumed to be a pure thrust against plate motion derived from MORVEL model ([DeMets et al., 2010, GJI](http://doi.org/10.1111/j.1365-246X.2009.04491.x)).
-- The codes are achieved with significant contribution from Amato Kasahara.
+- Codes for generating non-planar fault model by interpolating slab1.0 model ([Hayes et al., 2011, JGR](https://earthquake.usgs.gov/data/slab/)); which is used for inversion and/or backprojection analyses
+- Conversion between local cartesian and geographic coordinates is performed with [GeographicLib](http://geographiclib.sourceforge.net). [alex-robinson/coordinates](https://github.com/alex-robinson/coordinates)) is used for conversion between local cartesian and geographic coordinates.
+- Rake angle on each source knot is assumed to be a pure thrust against plate motion derived from MORVEL model ([DeMets et al., 2010, GJI](http://doi.org/10.1111/j.1365-246X.2009.04491.x)).
+- The codes are achieved by significant contribution from Amato Kasahara.
 
 ![](https://github.com/rokuwaki/FaultModSlab1.0/raw/master/work/FaultMod2006Kuril.png)
 
@@ -13,7 +13,7 @@
 $ bash bin/RunFaultModSlab.sh
 ```
 
-- Before executing script, edit input-parameters controlling fault model and slab1.0 models to be considered.
+- Before executing script, edit input-parameters on `bin/RunFaultModSlab.sh`.
 
 ## Dependencies
 
--- bin/RunFaultModSlab.sh
+++ bin/RunFaultModSlab.sh
@@ -1,32 +1,35 @@
 #!/usr/local/bin/bash
+mkdir -p data work
 
-lat0=46.592
-lon0=153.266
-
-mkdir -p data
+lat0=46.592  #lat for epicenter
+lon0=153.266 #lon for epicenter
+latp=48.9    #lat for angular velocity
+lonp=-71.7   #lon for angular velocity
+w=0.750      #angular velocity (DeMets et al., 2010, GJI, doi:10.1111/j.1365-246X.2009.04491.x)
+dl=2.0       #knot interval along strike
+dk=2.0       #knot interval along dip
+nl=151       #number of knots along `strike` (aparent strike for knot distribution)
+nk=151       #number of knots along `dip` (aparent dip for knot distribution)
+l0=76        #knot number along strike for hypocenter
+k0=76        #knot number along dip for hypocenter
+strf=225     #aparent strike angle for knot distribution
 Region=kur #alu, mex, cas, izu, ker, kur, phi, ryu, van, sco, sol, sam, sum
            #http://earthquake.usgs.gov/data/slab/
-wget https://earthquake.usgs.gov/data/slab/models/$Region"_slab1.0_clip.xyz"    -P data/
-wget https://earthquake.usgs.gov/data/slab/models/$Region"_slab1.0_strclip.xyz" -P data/
-wget https://earthquake.usgs.gov/data/slab/models/$Region"_slab1.0_dipclip.xyz" -P data/
-wget https://earthquake.usgs.gov/data/slab/models/$Region"_top.in"              -P data/
-
+#wget https://earthquake.usgs.gov/data/slab/models/$Region"_slab1.0_clip.xyz"    -P data/
+#wget https://earthquake.usgs.gov/data/slab/models/$Region"_slab1.0_strclip.xyz" -P data/
+#wget https://earthquake.usgs.gov/data/slab/models/$Region"_slab1.0_dipclip.xyz" -P data/
+#wget https://earthquake.usgs.gov/data/slab/models/$Region"_top.in"              -P data/
 depthdata='"data/'$Region'_slab1.0_clip.xyz"'
 strikedata='"data/'$Region'_slab1.0_strclip.xyz"'
 dipdata='"data/'$Region'_slab1.0_dipclip.xyz"'
 
-mkdir -p work
-echo $lat0 $lon0 "0"               > work/input.dat
-echo "48.9  -71.7  0.750"              >> work/input.dat
+echo $lat0 $lon0 "0"                    > work/input.dat
+echo $latp $lonp $w                    >> work/input.dat
 echo "2.0  2.0  151  151  76  76  225" >> work/input.dat
 echo $depthdata                        >> work/input.dat
 echo $strikedata                       >> work/input.dat
 echo $dipdata                          >> work/input.dat
 
-#lat lon dummy (must be 0) (epicenter)
-#lat lon w (Euler pole : NUVEL-1, DeMets et al., 2010, GJI)
-#dl dk nl nk l0 k0 strike of entire fault plane
-
 gfortran -o bin/FaultModSlab src/geodesic.f90 src/FaultModSlab.f90
 bin/FaultModSlab
-rm *.mod
+rm geodesic.mod
